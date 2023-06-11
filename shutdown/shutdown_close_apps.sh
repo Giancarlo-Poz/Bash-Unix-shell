613bbd7b14c355d2bba90653cdcbf3564e862e3f
Giancarlo-Poz <giancarlo.poz@gmail.com>
2023-06-11 23:05:57 +0700

    moved the instructions to delete backup files by simple tab groups from shutdown_close_apps.sh to shutdown_do.sh

--- shutdown/shutdown_close_apps.sh
+++ shutdown/shutdown_close_apps.sh
@@ -42,34 +42,5 @@ fi
 #printf " $(date '+%H:%M:%S') Firefox instructions finished\n|" >> ~/scriptsLog.log
 
 
-# Remove all exept last 3 backup files done by simple tab groups
-stg_bk_dir=$(find /home/giancarlo/Downloads/STG-backup*/ -type d)
 
-## print "remove following files" only if there are files to be removed
-for bk_dir in $stg_bk_dir;  
-do
-    file_to_be_deleted=$(find $bk_dir -type f | sort -t $'\t' -g | head -n -3 | cut -d $'\t' -f 2-)
-    if [ -n "$file_to_be_deleted" ]; then
-      printf "\n| $(date '+%H:%M:%S') remove following files\n" >> ~/scriptsLog.log
-      break
-    fi
-done
-
-## print the filenames to be removed and remove them
-for bk_dir in $stg_bk_dir; 
-do    
-    find $bk_dir -type f -printf '%T@\t%p\n' | sort -t $'\t' -g | head -n -3 | cut -d $'\t' -f 2- | sed -e 's/^/| | /' >> ~/scriptsLog.log 
-    find $bk_dir -type f -printf '%T@\t%p\n' | sort -t $'\t' -g | head -n -3 | cut -d $'\t' -f 2- | xargs -d'\n' rm -f
-done
-
-
-# find /home/giancarlo/Downloads/TabSessionManager-Backup/ -type f -printf '%T@\t%p\n' | sort -t $'\t' -g | head -n -2 | cut -d $'\t' -f 2- | xargs -d'\n' rm -f # leave only 2 files
-# find /home/giancarlo/Downloads/TabSessionManager-Backup/ -type f -printf '%T@\t%p\n' | sort -t $'\t' -g | head -n -2 | cut -d $'\t' -f 2- | sed "s/\ /\\\ /g" | xargs rm -f
-
-#find /home/giancarlo/Downloads/STG-backups-FF-*/ -type f -printf '%T@\t%p\n' | # lists all files in STG backup directory. They are printed out with timestamps
-#sort -t $'\t' -g | # sorts the lines based on timestamp (oldest on top)
-#head -n -3 | # prints out the top lines, up to the last 3 lines
-#cut -d $'\t' -f 2- | # removes the timestamps
-#sed "s/\ /\\\ /g" | # substitute white spaces " " with slashed withe spaces "\ "
-#xargs -d'\n' rm -f # runs rm for every selected file. the \n delimites the different files
 
