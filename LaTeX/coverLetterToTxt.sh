7ab6ea1857cce800d9745d00b1cdced4eb2a5321
Giancarlo-Poz <giancarlo.poz@gmail.com>
2023-07-06 22:53:13 +0700

    fixed: after deleting new lines last and first word of line are joined

--- LaTeX/coverLetterToTxt.sh
+++ LaTeX/coverLetterToTxt.sh
@@ -7,8 +7,15 @@ pdftotext coverLetter_GiancarloPozzo.pdf #convert to txt
 sed -i '1,10d' coverLetter_GiancarloPozzo.txt #delete first 10 lines
 sed -i '$ d' coverLetter_GiancarloPozzo.txt #delete last line (because there is a unwanted character
 
-#delete all new lines
-tr -d '\n' < coverLetter_GiancarloPozzo.txt > coverLetter_GiancarloPozzo1.txt
+# tr(anslate) new lines into a space. -s(queeze) double spaces into 1 spaces
+tr -s '\n' ' ' < coverLetter_GiancarloPozzo.txt > coverLetter_GiancarloPozzo1.txt
 mv coverLetter_GiancarloPozzo1.txt coverLetter_GiancarloPozzo.txt
+
+#insert new lines
+#sed -i 's/Dear Hiring Team,/&\n\n/g' coverLetter_GiancarloPozzo.txt
+#sed -i '0,/Dear Hiring Team, /s//&\n\n/' coverLetter_GiancarloPozzo.txt
+#sed -i 's/substring/&\n\n/g' file.txt
+
+#future plans.
 
 cd "$pwd"
