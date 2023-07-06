eb4d517edae7e282dffd174e884403cabc8a4393
Giancarlo-Poz <giancarlo.poz@gmail.com>
2023-07-06 22:32:00 +0700

    delete all new lines

--- LaTeX/coverLetterToTxt.sh
+++ LaTeX/coverLetterToTxt.sh
@@ -7,4 +7,8 @@ pdftotext coverLetter_GiancarloPozzo.pdf #convert to txt
 sed -i '1,10d' coverLetter_GiancarloPozzo.txt #delete first 10 lines
 sed -i '$ d' coverLetter_GiancarloPozzo.txt #delete last line (because there is a unwanted character
 
+#delete all new lines
+tr -d '\n' < coverLetter_GiancarloPozzo.txt > coverLetter_GiancarloPozzo1.txt
+mv coverLetter_GiancarloPozzo1.txt coverLetter_GiancarloPozzo.txt
+
 cd "$pwd"
