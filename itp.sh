3874caf9d8a744a860cd03cb1c08f30df182353b
Giancarlo-Poz <giancarlo.poz@gmail.com>
2023-07-08 02:44:42 +0700

    added functions

--- itp.sh
+++ itp.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
-#define the function
-calc(){ awk "BEGIN { print $*}"; }
+# define the function to perform calculations
+calc() { awk "BEGIN { print $*}"; }
 
+#define sign function
+sign() {
+  awk "BEGIN { 
+    if ($1 > 0) 
+      print 1; 
+    else if ($1 < 0) 
+      print -1; 
+    else 
+      print 0 
+    }"
+}
+
+
+
+#define absolute value function
+abs() {
+  awk -v num="$1" "BEGIN { 
+    if (num < 0) 
+      num = -1*num; 
+    print num 
+    }"
+}
+
+##define ceiling function
+#ceil() {
+#$(awk "BEGIN { printf \"%.0f\", int($x + ($x > int($x))) }"
+#}
+
+ceil() {
+    awk -v num="$1" 'BEGIN { ceiling = int(num + (num > int(num))); print ceiling }'
+}
+
+echo $(abs "83.21")
