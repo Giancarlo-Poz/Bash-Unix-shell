c1db271e3813cdf47373e46bbd27d9d7696eac95
Giancarlo-Poz <giancarlo.poz@gmail.com>
2023-11-04 22:19:39 +0700

    added descriptions

--- ITP/user.sh
+++ ITP/user.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Variable
-x_best=$1
+# Variable 
+# Must be an element of real numbers (or an element of a subset of real numbers)
+variable=$1
 
-# Function (compress a pdf)
+# These falues are defined here but read by another file
+# Change only the right hand side according to your specification
+# Do not change the left hand side
+variable_min=30
+variable_max=100
+
+# Function. Can be whatever you want (eg. compress a pdf)
 input_file=$2
 output_file="compressed_${input_file}"
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dColorImageResolution=$x_best -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$output_file $input_file
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dColorImageResolution=$variable -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$output_file $input_file
 
 # Output
+# Must be an element of real numbers (or an element of a subset of real numbers)
 size=$(stat -c %s "$output_file")
 echo $size
