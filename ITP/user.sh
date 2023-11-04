985ba03ace961957a337b08abb9971f4e17eef8b
Giancarlo-Poz <giancarlo.poz@gmail.com>
2023-11-05 00:06:56 +0700

    not important: changed description

--- ITP/user.sh
+++ ITP/user.sh
@@ -4,12 +4,16 @@
 # Must be an element of real numbers (or an element of a subset of real numbers)
 variable=$1
 
-
-# These falues are defined here but read by another file
-# Change only the right hand side according to your specification
-# Do not change the left hand side
+# Change only the value (right hand side) according to your specification
+# Do not change the variable name (left hand side)
 variable_min=30
 variable_max=100
+
+
+# TO DO CHECK posix compliance? is it the right first try according to ITP?
+# Do not change
+first_try=$(( ($variable_max - $variable_min)/2 )) # TO CHANGE
+variable=${variable:-$first_try}
 
 
 # Function. Can be whatever you want (eg. compress a pdf)
@@ -20,6 +24,9 @@ gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dColorImage
 
 
 # Output
+# Do not change the variable name (left hand side)
 # Must be an element of real numbers (or an element of a subset of real numbers)
-size=$(stat -c %s "$output_file")
-echo $size
+output=$(stat -c %s "$output_file")
+
+# Do not change the following line
+echo $variable_min $variable_max $output
